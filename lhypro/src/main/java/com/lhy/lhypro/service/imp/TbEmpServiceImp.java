df8b7828c69fd5a4488ae0f19e2ec1673b598086
lhy <15201151025@163.com>
2020-04-11 16:16:53 +0800

    redis锁

--- lhypro/src/main/java/com/lhy/lhypro/service/imp/TbEmpServiceImp.java
+++ lhypro/src/main/java/com/lhy/lhypro/service/imp/TbEmpServiceImp.java
@@ -6,7 +6,11 @@ import com.lhy.lhypro.po.TbEmp;
 import com.lhy.lhypro.service.RedisCacheService;
 import com.lhy.lhypro.service.TbEmpService;
 import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.annotation.Before;
+import org.redisson.api.RLock;
+import org.redisson.api.RedissonClient;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -20,6 +24,12 @@ public class TbEmpServiceImp implements TbEmpService {
 
     @Autowired
     private RedisCacheService redisCacheService;
+
+    @Autowired
+    private RedissonClient redissonClient;
+
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
 
     public ResponseEntity<TbEmp> findPage(PageBounds pageBounds) {
         return new ResponseEntity<>(dao.selectByPrimaryKey(1), HttpStatus.OK);
@@ -39,5 +49,31 @@ public class TbEmpServiceImp implements TbEmpService {
 
     public TbEmp findeEntityById(Integer userId) {
         return dao.selectByPrimaryKey(userId);
+    }
+
+    /**
+     *@Author: lihuiyang
+     *@Date: 3:29 下午 2020/4/11
+     *@Description: redis锁
+    */
+    public void redisLock(){
+        stringRedisTemplate.opsForValue().set("stock","10");
+        String lockKey = "lockKey";
+        RLock lock = redissonClient.getLock(lockKey);
+        try {
+            lock.lock();
+            int stock = Integer.parseInt(stringRedisTemplate.opsForValue().get("stock"));
+            if(stock > 0){
+                int overage = stock - 1;
+                stringRedisTemplate.opsForValue().set("stock",String.valueOf(overage));
+                log.info("扣减成功，库存stock：{}",overage);
+            }else {
+                log.info("扣减失败，库存不足");
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }finally {
+            lock.unlock();
+        }
     }
 }
---
+++ lhypro/src/test/java/test/Test2.java
@@ -0,0 +1,52 @@
+package test;
+
+/**
+ * 项目名字：insurance-mall
+ *
+ * @author 作者: lihuiyang 功能描述：1. 创建时间: 2020/4/5 所属公司：sinosoft
+ */
+public class Test2 {
+    public static void main(String[] args) {
+        Node node = new Node(0);
+        Node node1 = new Node(1);
+        Node node2 = new Node(2);
+        Node node3 = new Node(3);
+        node.setNext(node1);
+        node1.setNext(node2);
+        node2.setNext(node3);
+        System.out.println(node);
+        ss(node);
+        System.out.println(node);
+    }
+
+    public static void ss(Node node){
+        Node curr = node.next;
+        Node next = null;
+        Node newN = new Node(10);
+        while (curr != null){
+            next = curr.next;
+            curr.next = newN.next;
+            newN.next = curr;
+            curr = next;
+        }
+        node.next = newN.next;
+    }
+}
+class Node {
+    public int id;
+    public Node next;
+    public Node(int id){
+        this.id = id;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
+
+    @Override
+    public String toString() {
+        return "Node{" +
+                "id=" + id +
+                "}";
+    }
+}
