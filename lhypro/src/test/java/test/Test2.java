35bbd757ffcfcb799e3d21b4cc4e4c4b9f87d028
lhy <15201151025@163.com>
2020-04-15 14:39:27 +0800

    HashMap学习

--- lhypro/src/test/java/test/Test2.java
+++ lhypro/src/test/java/test/Test2.java
@@ -1,9 +1,9 @@
 package test;
 
 /**
- * 项目名字：insurance-mall
+ * 项目名字：lhy
  *
- * @author 作者: lihuiyang 功能描述：1. 创建时间: 2020/4/5 所属公司：sinosoft
+ * @author 作者: lihuiyang 功能描述：1. 创建时间: 2020/4/5 所属公司：lhy
  */
 public class Test2 {
     public static void main(String[] args) {
